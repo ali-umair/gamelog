6c6b11ff0f22d064d4a4d69ecc889044bf325ae5
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-15 10:32:26 +0500

    Using node(axios) now instead of fetch

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-fetch("https://api.igdb.com/v4/covers", {
+const axios = require('axios');
+
+
+
+axios({
+    url: 'https://api.igdb.com/v4/covers',
     method: 'POST',
     headers: {
         'Accept': 'application/json',
@@ -6,7 +11,7 @@ fetch("https://api.igdb.com/v4/covers", {
         'Authorization': 'Bearer 0s4tl8fzig62mj4ww01l6u1e5r22gm',
     },
     data: "fields image_id, url;limit 50;offset 0;"
-  })
+})
     .then(response => {
         console.log(response.data);
     })
